aeb03012910b8fe7c01ed08b99eba1dc0b66b7f2
Aditya Kumar Singh <iamaksingh2004@gmail.com>
2025-11-23 19:17:30 +0530

    updared Db config to support v7 by removing url parser and unified topology

--- config/db.js
+++ config/db.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 module.exports = async function connectDB() {
   const uri = process.env.MONGODB_URI;
   if (!uri) throw new Error('MONGODB_URI not set in env');
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(uri);
   console.log('Connected to MongoDB');
 };
